80f901cae9066406d7c9dd4ac1a604f2b8c51265
David <dev.davidperez@gmail.com>
2021-06-28 11:17:44 +0200

    initial commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>DarkImmortal</title>
+        <link rel="stylesheet" href="css\layout.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Nosifer&family=Roboto&display=swap" rel="stylesheet">   
+
+    </head>
+    <body>
+        <div class="sitename">
+            <h1>DARKIMMORTAL</h1>
+        </div>
+        <nav>
+            <ul class="liwrapper">
+                <li><a href="{{ route('groupes') }}">Groupes</a></li>
+                <li><a href="#">Sons</a></li>
+                <li><a href="#">Clips</a></li>
+                <li><a href="#">Iconographies</a></li>
+                <li><a href="{{ route('about') }}">À propos</a></li>
+            </ul>
+        </nav>
+        <div class='container'>
+            @yield('content')
+        </div>
+        
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.menu')
+
+@section('content')
+tous les articles
+@endsection
